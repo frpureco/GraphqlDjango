dc31ef4279036c2cbeb47cb5f5de30c336299ab4
frpureco <fpurecol@gmail.com>
2019-10-16 23:52:24 -0500

    0.0.6  git added with commit -m

--- pokedex/schema.py
+++ pokedex/schema.py
@@ -2,7 +2,7 @@ from graphene import relay
 from graphene_django.filter import DjangoFilterConnectionField
 from pokedex.nodes import PokemonNode
 
-
+# est es un nuevo comentario
 class Query (object):
     pokemon = relay.Node.Field(PokemonNode)
     pokemons = DjangoFilterConnectionField(PokemonNode)
