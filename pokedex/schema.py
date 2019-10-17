d3b750050c3f5c9e6c951ac46b508224e49e6c81
frpureco <fpurecol@gmail.com>
2019-10-16 23:39:14 -0500

    version 0.0.5 version complited

---
+++ pokedex/schema.py
@@ -0,0 +1,12 @@
+from graphene import relay
+from graphene_django.filter import DjangoFilterConnectionField
+from pokedex.nodes import PokemonNode
+
+
+class Query (object):
+    pokemon = relay.Node.Field(PokemonNode)
+    pokemons = DjangoFilterConnectionField(PokemonNode)
+
+class Mutation(object):
+    pass
+
